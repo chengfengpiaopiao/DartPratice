ce5ddc01a6735c41a41acb988099eaa448c790f6
chengfengpiaopiao <13574845807@163.com>
2019-02-15 06:46:50 +0800

    first commit

---
+++ bin/main.dart.com/0_Variable/variable.dart
@@ -0,0 +1,12 @@
+
+class VariableTest{
+
+  void testVariable(){
+     print("---------------------split line---------------------");
+     // 变量申明和初始化操作
+     Object srcObj = "JiangWei";
+     print(srcObj);
+
+  }
+}
+
---
+++ bin/main.dart.com/dart.dart
@@ -0,0 +1,11 @@
+//import 'package:css_colors/css_colors.dart'; //导入包
+import '0_Variable/variable.dart';
+void main() {
+  print('Hello, World!');
+
+  /**
+   * 单元测试
+   */
+  var variableTest = new VariableTest();
+  variableTest.testVariable();
+}
